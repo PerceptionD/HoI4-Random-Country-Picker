65765555f37a965635d12110f92dd66e7872e3f3
PerceptionD <PerceptionD@users.noreply.github.com>
2019-07-27 12:43:59 +0000

    Replaced tabs with spaces

--- support.cc
+++ support.cc
@@ -1,21 +1,21 @@
 #include "support.h"
 
 void populate_list(std::vector<std::string>& countryList, std::string fileName) {
-	std::ifstream ist { fileName };
-	std::string name;
-	while (ist>>name)
-		countryList.push_back(name);
+  std::ifstream ist { fileName };
+  std::string name;
+  while (ist>>name)
+    countryList.push_back(name);
 }
 
 bool ask_question(std::string question) {
-	std::string ch;
-	std::cout << question << std::endl;
-	while (std::cin >> ch) {
-	  if (ch=="y"||ch=="n"||ch=="Y"||ch=="N") break;
-	  std::cout << "Please answer with y or n." << std::endl;
-	}
-	if (ch=="y"||ch=="Y") return true;
-	return false;
+  std::string ch;
+  std::cout << question << std::endl;
+  while (std::cin >> ch) {
+    if (ch=="y"||ch=="n"||ch=="Y"||ch=="N") break;
+    std::cout << "Please answer with y or n." << std::endl;
+  }
+  if (ch=="y"||ch=="Y") return true;
+  return false;
 }
 
 std::string remove_underscore(std::string phrase) {
