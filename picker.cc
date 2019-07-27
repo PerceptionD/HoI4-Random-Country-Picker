ecda86cc65c62a7faebd0d3e8b513289ccd24ed1
PerceptionD <PerceptionD@users.noreply.github.com>
2019-07-27 13:55:09 +0000

    Added random ideology

--- picker.cc
+++ picker.cc
@@ -3,15 +3,28 @@
 int main() {
   bool releasable = ask_question(
       "Would you like to include releasable countries? (y/n) ");
+
   std::vector<std::string> countryVec;
-  if (releasable) populate_list(countryVec, "releasable");
+  std::vector<std::string> ideologyVec;
+  std::vector<std::string> nonalignable;
+  std::string ideology;
+
+  if (releasable) 
+    populate_list(countryVec, "releasable");
   populate_list(countryVec, "countrylist");
-  std::random_device rannum;
-  std::mt19937 rng(rannum());
-  std::uniform_int_distribution<std::mt19937::result_type> 
-      dist(0, countryVec.size()-1);
-  std::cout << "Country: " << remove_underscore(countryVec[dist(rng)]) 
-      << std::endl;
+  populate_list(ideologyVec, "ideology");
+  populate_list(nonalignable, "nonalignable");
+
+  srand(time(NULL));
+  std::string country = countryVec[rand() % countryVec.size()];
+  std::cout << "Country: " << remove_underscore(country) << std::endl;
+
+  if (std::count(nonalignable.begin(), nonalignable.end(), country) == 0)
+    ideologyVec.erase(std::remove(ideologyVec.begin(), ideologyVec.end(),
+        "Non-Aligned"), ideologyVec.end());
+  ideology = ideologyVec[rand() % ideologyVec.size()];
+
+  std::cout << "Ideology: " << ideology << std::endl;
   std::cin.ignore(1);  // clear cin stream and keep the window open for input
   std::cin.get();
   return 0;
